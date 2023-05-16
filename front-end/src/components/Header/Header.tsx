4642732f9feb414e1a83fc317d0dd1eede77b202
Milan-sandeepa <glmsandeepa@gmail.com>
2023-05-16 20:38:03 +0530

    Header updated

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -10,29 +10,88 @@ import Diversity1RoundedIcon from "@mui/icons-material/Diversity1Rounded";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
 import ChatBubbleRoundedIcon from "@mui/icons-material/ChatBubbleRounded";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 
 export default class Header extends Component {
   render() {
-    const active = "border-b-2 cursor-pointer";
+    const active = "border-b-2 pb-[23px] border-blue-navy-200 cursor-pointer";
     const normalLink = "cursor-ponter";
 
     return (
       <div className="bg-[#FFFFFF] drop-shadow h-16 w-full fixed top-0 ">
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between ">
           <div className="p-0 flex items-center">
-            <img className="w-[200px]" src={logo} alt="logo" />
+            <NavLink to={"/home"}>
+              <img className="w-[200px]" src={logo} alt="logo" />
+            </NavLink>
           </div>
-          <div className="p-0 flex items-center gap-20">
-            <HomeRoundedIcon />
-            <ElectricBoltRoundedIcon />
-            <VideoCameraBackRoundedIcon />
-            <Diversity1RoundedIcon />
+          <div className="w-1/3 mt-[14px]">
+            <nav className="flex justify-between gap-28">
+              {/* <form action="">
+                <TextField
+                  id="search-bar"
+                  className="text"
+                  // onInput={(e) => {
+                  //   );
+                  // }}
+                  label="Start typing to search"
+                  variant="outlined"
+                  placeholder="Search..."
+                  size="small"
+                />
+                <IconButton type="submit" aria-label="search">
+                  <SearchIcon style={{ fill: "blue" }} />
+                </IconButton>
+              </form> */}
+              <NavLink
+                to={"/home"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <HomeRoundedIcon />
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <ElectricBoltRoundedIcon />
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <VideoCameraBackRoundedIcon />
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <Diversity1RoundedIcon />
+              </NavLink>
+            </nav>
           </div>
-          <div className="p-0 pe-5 flex items-center gap-10">
-            <NotificationsRoundedIcon />
-            <ChatBubbleRoundedIcon />
-            <AccountCircleRoundedIcon />
-            <img className="" src={profile} alt="pro" />
+          <div className="p-0 pe-5 w-1/3 mt-[14px]">
+            <nav className="flex  justify-end gap-10 ">
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <NotificationsRoundedIcon />
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <ChatBubbleRoundedIcon />
+              </NavLink>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? active : normalLink)}
+              >
+                <AccountCircleRoundedIcon />
+              </NavLink>
+            </nav>
           </div>
         </div>
       </div>
